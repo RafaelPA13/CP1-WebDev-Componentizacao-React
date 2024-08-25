5a60ed26f0f25748b24b1ad8f70145129ddfc4c1
Jenismoraes <jenimoraeslima89@gmail.com>
2024-08-25 17:36:50 -0300

    Conteudo contato atualizado.2

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -1,5 +1,31 @@
 import './styles.css';
+import Button from "../../components/button";
 
 export default function Contato() {
-  return <div><h1>Dúvidas e suporte, entre em contato:</h1></div>;
+  return (
+    <>
+      <header className="banner-contato">
+        <div className="contato-texto">
+          <h1>Dúvidas e suporte, entre em contato:</h1>
+        </div>
+      </header>
+
+      <div className="main-contato">
+        <div className="inputs-contato">
+          <div className="form-group">
+            <label>Nome:</label>
+            <input type="text" id="nome" name="nome" required />
+          </div>
+          <div className="form-group">
+            <label>E-mail:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label>Mensagem:</label>
+          </div>
+          <Button texto={"Enviar"} />
+        </div>
+      </div>
+    </>
+  );
 }
