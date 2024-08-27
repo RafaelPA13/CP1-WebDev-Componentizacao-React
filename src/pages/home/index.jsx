ba058e29ee4f516868aa582bab7a67d6ae842c6a
Matheus Diniz <matheusdiniz9@outlook.com>
2024-08-27 19:27:11 -0300

    pequenas correcoes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,14 +4,14 @@ import Button from '../../components/button';
 export default function Home() {
   return (
     <section class="hero">
-    <div class="hero-text">
+      <div class="hero-text">
         <h1>Crie seus vídeos online</h1>
         <p>Transforme suas ideias em filmes memoráveis: onde a criatividade encontra a simplicidade.</p>
-        <Button texto={"Começar agora!!"}/>
-    </div>
-    <div class="cara_saltando">
-        <img src="/images/banners/cara_saltando.png" alt="Imagem"/>
-    </div>
-</section>
+        <Button texto="Começar agora!" />
+      </div>
+      <div class="cara_saltando">
+        <img src="/images/banners/cara_saltando.png" alt="Banner Home"/>
+      </div>
+    </section>
   );
 }
