3442cd382e90fa8b2ca232d76f9a460d2c66b759
Jenismoraes <jenimoraeslima89@gmail.com>
2024-08-27 20:56:33 -0300

    começando a add css

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -5,27 +5,19 @@ export default function Contato() {
   return (
     <>
       <header className="banner-contato">
-        <div className="contato-texto">
-          <h1>Dúvidas e suporte, entre em contato:</h1>
-        </div>
-      </header>
+          <div className="contato-texto">
+            <h1>Dúvidas e suporte, entre em contato:</h1>
+          </div>
+        </header>
 
-      <div className="main-contato">
-        <div className="inputs-contato">
-          <div className="form-group">
-            <label>Nome:</label>
-            <input type="text" id="nome" name="nome" required />
-          </div>
-          <div className="form-group">
-            <label>E-mail:</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <div className="form-group">
-            <label>Mensagem:</label>
-          </div>
+      <section id="contact">
+        <form>
+          <input type="text" placeholder="Nome" />
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Mensagem" />
           <Button texto={"Enviar"} />
-        </div>
-      </div>
+        </form>
+      </section>
     </>
   );
 }
