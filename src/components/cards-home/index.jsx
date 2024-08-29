1d0a2f53b98057ebc3ff5c2a2ca36c8df660527b
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-08-29 10:36:55 -0300

    adicao do cards na pagina home

---
+++ src/components/cards-home/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function Cards_Home () {
+    return (
+        <>
+            <h1>Cards home</h1>
+        </>
+    )
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,20 @@
 import './styles.css';
 import Button from '../../components/button';
+import Cards_Home from '../../components/cards-home';
 
 export default function Home() {
   return (
-    <section className="hero">
+    <><section className="hero">
       <div className="hero-text">
         <h1>Crie seus vídeos online</h1>
         <p>Transforme suas ideias em filmes memoráveis: onde a criatividade encontra a simplicidade.</p>
         <Button texto="Começar agora!" />
       </div>
       <div className="cara_saltando">
-        <img src="/images/banners/cara_saltando.png" alt="Banner Home"/>
+        <img src="/images/banners/cara_saltando.png" alt="Banner Home" />
       </div>
-    </section>
+    </section><section>
+        <Cards_Home />
+      </section></>
   );
 }
