829accd85697dfda09dcfe110f27b29db83cf716
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-08-29 12:02:50 -0300

    cards finalizados

--- src/components/cards-home/index.jsx
+++ src/components/cards-home/index.jsx
@@ -1,9 +1,16 @@
 import './styles.css'
 
-export default function Cards_Home () {
+export default function Cards_Home ({ cor, numero, titulo, icone, alt, descricao }) {
     return (
         <>
-            <h1>Cards home</h1>
+            <div className="cards-home" style={{ backgroundColor: cor }}>
+                <h1>{numero}</h1>
+                <h3>{titulo}</h3>
+                <div className="img-container">
+                    <img src={icone} alt={alt} />
+                </div>
+                <p>{descricao}</p>
+            </div>
         </>
     )
 }
