2a0240b1c28cf0d8ee3f9745a37fb449a0e4e006
Jenismoraes <jenimoraeslima89@gmail.com>
2024-08-29 10:00:46 -0300

    finalizando html e css

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -1,12 +1,25 @@
 import './styles.css';
-import '../../App.css'
 
 export default function Contato() {
   return (
     <>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
+    </style>
       <div className="container">
         <div className="contato-texto">
           <h1>Dúvidas e suporte, entre em contato:</h1>
+          <div className='icons'>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/icons/twitter_icon.png" alt="Icone X" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/icons/instagram2_icon.png" alt="Icone Instagram" />
+            </a>
+            <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+              <img src="/images/icons/discord_icon.png" alt="Icone Discord" />
+            </a>
+          </div>
         </div>
         <div id="contact">
           <form>
