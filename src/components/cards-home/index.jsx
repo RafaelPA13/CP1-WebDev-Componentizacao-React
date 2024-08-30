f1509033f5cbf5da7ad82fc061dc72bb809850e1
Matheus Diniz <mtsdnz@hotmail.com>
2024-08-30 10:05:03 -0300

    Corrigido alinhamento

--- src/components/cards-home/index.jsx
+++ src/components/cards-home/index.jsx
@@ -1,16 +1,21 @@
-import './styles.css'
+import './styles.css';
 
-export default function Cards_Home ({ cor, numero, titulo, icone, alt, descricao }) {
-    return (
-        <>
-            <div className="cards-home" style={{ backgroundColor: cor }}>
-                <h1>{numero}</h1>
-                <h3>{titulo}</h3>
-                <div className="img-container">
-                    <img src={icone} alt={alt} />
-                </div>
-                <p>{descricao}</p>
-            </div>
-        </>
-    )
+export default function Cards_Home({
+  cor,
+  numero,
+  titulo,
+  icone,
+  alt,
+  descricao,
+}) {
+  return (
+    <div className="cards-home" style={{ backgroundColor: cor }}>
+      <h1>{numero}</h1>
+      <h3>{titulo}</h3>
+      <div className="img-container">
+        <img src={icone} alt={alt} />
+      </div>
+      <p>{descricao}</p>
+    </div>
+  );
 }
